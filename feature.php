1325c6607f0c10e9e8a85033c00120aca8c41f1b
John Conrad Lumacad <jcmlumacad@tmj.ph>
2017-03-07 12:35:38 +0800

    add feature file

---
+++ feature.php
@@ -0,0 +1,14 @@
+<?php
+
+class Feature
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    public function featureTwo()
+    {
+        return 'Feature Two';
+    }
+}
